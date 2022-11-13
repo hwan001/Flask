07b27f17b3af67e75a9a16519bf7aa8057dbd6f2
hwan001 <woghks7209@gmail.com>
2022-11-13 23:47:30 +0900

    update minitter

--- minitter/web/app.py
+++ minitter/web/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, request, jsonify
+from flask.json import JSONEncoder
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return JSONEncoder.default(self, obj)
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
 app.users = {}
 app.id_count = 1
 app.tweets = []
+app.follow = {}
 
 def create_app():
     return app
@@ -37,4 +46,44 @@ def tweet():
     
     return '', 200
 
-#@app.route("", methods=[])
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+    
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "사용자가 존재하지 않습니다.", 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+
+    return jsonify(user)
+
+@app.route("/unfollow", methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['unfollow'])
+    
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "사용자가 존재하지 않습니다.", 400
+    
+    user = app.users[user_id]
+    user.setdefault('follow', set()).discard(user_id_to_follow)
+
+    return jsonify(user)
+
+@app.route("/timeline/<int:user_id>", methods=['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return "사용자가 존재하지 않습니다.", 400
+    
+    follow_list = app.users[user_id].get("follow", set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+            "user_id":user_id,
+            "timeline":timeline
+    })
